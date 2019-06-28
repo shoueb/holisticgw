83a994803ce680e4138365b2cf5c3ec212e8144d
yuangui.hu <yuangui.hu@luckincoffee.com>
2019-06-28 15:09:13 +0800

    Add: 投资页面兼容WAP

--- src/main/java/com/bootdo/pc/wap/InvestmentPromotionWapController.java
+++ src/main/java/com/bootdo/pc/wap/InvestmentPromotionWapController.java
@@ -1,19 +1,20 @@
-package com.bootdo.pc.web;
+package com.bootdo.pc.wap;
 
 import com.bootdo.pc.commcontroller.InvestmentCommController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by huyuangui on 2018/8/11.
  */
 @Controller
-@RequestMapping("/pc")
-public class InvestmentPromotionController extends InvestmentCommController {
+@RequestMapping("/wap")
+public class InvestmentPromotionWapController extends InvestmentCommController {
     @RequestMapping("investmentPromotion")
     @Override
     public String index(){
         super.index();
-        return "pc/web/investmentPromotion";
+        return "pc/wap/investmentPromotion";
     }
 }
 
